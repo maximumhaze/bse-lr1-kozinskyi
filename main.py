d801bc1dd15131ca1b433a006c27da8a62ac0095
Козінський Ілля <illia.kozinskyi@nure.ua>
2026-05-18 23:08:01 +0300

    fix: виправлено кодування у файлі main.py на UTF-8

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-
-def calculate_area(length, width):
-    return length * width
-
-print("����� ������ 5�4 �����:", calculate_area(5, 4))
-# Функція для розрахунку бюджету подорожі (FR-05). 
-# Приймає: кількість днів, вартість готелю за день, витрати на їжу за день. 
-# Повертає загальну суму.
-
-def  calculate_travel_budget(days, hotel_cost_per_day, food_cost_per_day):
+def calculate_travel_budget(days, hotel_cost_per_day, food_cost_per_day):
     total_hotel_cost = days * hotel_cost_per_day
     total_food_cost = days * food_cost_per_day
     total_budget = total_hotel_cost + total_food_cost
     return total_budget
 
-# Тестування функції
 budget = calculate_travel_budget(7, 100, 50)
 print("Орієнтовний бюджет подорожі:", budget)
-
